897d06a3d9a61aae7742ea1a3cf7c77b7dea7ae5
Evgeniy <redlightexit@gmail.com>
2019-06-18 00:17:05 +0900

    Add files via upload

---
+++ les_1_task_2.py
@@ -0,0 +1,9 @@
+bytes1 = b'class'
+bytes2 = b'function'
+bytes3 = b'method'
+bytes1_str = bytes1.decode('utf-8')
+bytes2_str = bytes2.decode('utf-8')
+bytes3_str = bytes3.decode('utf-8')
+print(bytes1_str, type(bytes1_str))
+print(bytes2_str, type(bytes2_str))
+print(bytes3_str, type(bytes3_str))
